e334081df163070422c1dfe7b1911c59cb118d51
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2026-06-20 13:23:27 -0400

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#FUNCTIONS GO HERE
-
 confirm() {         
     while true; do
         read -p "${1}" yn
@@ -12,35 +10,11 @@ confirm() {
         esac
     done
 }
-example-function() {
-    echo "Excellent. You haven't broken it. Yet."
-}
-    #Generate locales
-ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-hwclock --systohc
-echo "en_US.UTF-8 UTF-8">> /etc/locale.gen
-locale-gen 
-confirm "Did locales generate?"
-echo "LANG=en_US.UTF-8
-LC_ALL=en_US.UTF-8
-LC_ADDRESS=en_US.UTF-8
-LC_NAME=en_US.UTF-8
-LC_MONETARY=en_US.UTF-8
-LC_PAPER=en_US.UTF-8
-LC_IDENTIFICATION=en_US.UTF-8
-LC_TELEPHONE=en_US.UTF-8
-LC_MEASUREMENT=en_US.UTF-8
-LC_TIME=en_US.UTF-8
-LC_NUMERIC=en_US.UTF-8
-LANGUAGE=en_US.UTF-8">> /etc/locale.conf
-echo "KEYMAP=us
-FONT=Lat2-Terminus16">> /etc/vconsole.conf
-#echo "ballerina">> /etc/hostname
 
 #set the device hostname
 read -p "What is the hostname for this device?" host
 confirm "Is "$host" correct?"
-echo "$host">> /etc/hostname
+echo "$host" >> /etc/hostname
 cat /etc/hostname
 confirm "Was the hostname set correctly?"
 
@@ -53,10 +27,10 @@ echo "
 
 #add yourself as a user
 read -p "What is your username going to be?" username
-confirm "Is "$username" correct?"
-useradd -m -G wheel -s /bin/bash “$username”
-passwd “$username”
+confirm "Is $username correct?"
+useradd -m -G wheel -s /bin/bash $username
+passwd $username
 groupadd fuse
-usermod -a -G fuse “$username”    
-cat /etc/passwd
+usermod -a -G fuse $username 
+ls /home/
 confirm "Was the user set correctly?
