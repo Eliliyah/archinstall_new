57898ed67f01730a8302e52433e49e79a3e44d33
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2026-06-20 12:19:16 -0400

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -10,17 +10,20 @@ confirm() {
         esac
     done
 }
-example-function() {
-    echo "$2"
-}
 
-#Choose the drive to repair
+#Choose the drive for installation
 lsblk
-read -p "What is the name of your first (boot) partition?" boot
-confirm "Is "$boot" correct?"
+read -p "Which device will you be chrooting into? Please specify the full device path." dev_block
+confirm "Is "$dev_block" correct?"
 
-read -p "What is the name of your second (swap) partition?" swap
-confirm "Is "$swap" correct?"
+read -p "What is the name of your first (boot) partition?" bootpart
+confirm "Is $bootpart" correct?
+
+read -p "What is the name of your second (swap) partition?" swappy
+confirm "Is $swappy" correct?
+
+read -p "What is the name of your third (root) partition?" rootpart
+confirm "Is $rootpart" correct?
 
 #Mount the partitions
 o=defaults,x-mount.mkdir
@@ -32,7 +35,7 @@ mount -t btrfs -o subvol=@srv,$o_btrfs LABEL=system /mnt/srv
 mount -t btrfs -o subvol=@log,$o_btrfs LABEL=system /mnt/var/log
 mount -t btrfs -o subvol=@tmp,$o_btrfs LABEL=system /mnt/var/tmp
 mount -t btrfs -o subvol=@cache,$o_btrfs LABEL=system /mnt/var/cache
-mount "$boot" /mnt/boot
-swapon "$swap"
+mount "$bootpart" /mnt/boot
+swapon "$swappy"
 btrfs quota enable /mnt
 lsblk
