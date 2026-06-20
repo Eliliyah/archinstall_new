fe554e3f86e72a96210befaf485f2f565356bf46
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2026-06-20 13:20:57 -0400

    Create locales.sh

---
+++ locales.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+confirm() {         
+    while true; do
+        read -p "${1}" yn
+        case $yn in
+            [Yy]* ) $2; break;;
+            [Nn]* ) echo "aborted"; exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+#Generate locales
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8">> /etc/locale.gen
+locale-gen 
+confirm "Did locales generate?"
+echo "LANG=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+LC_ADDRESS=en_US.UTF-8
+LC_NAME=en_US.UTF-8
+LC_MONETARY=en_US.UTF-8
+LC_PAPER=en_US.UTF-8
+LC_IDENTIFICATION=en_US.UTF-8
+LC_TELEPHONE=en_US.UTF-8
+LC_MEASUREMENT=en_US.UTF-8
+LC_TIME=en_US.UTF-8
+LC_NUMERIC=en_US.UTF-8
+LANGUAGE=en_US.UTF-8">> /etc/locale.conf
+echo "KEYMAP=us
+FONT=Lat2-Terminus16">> /etc/vconsole.conf
+
